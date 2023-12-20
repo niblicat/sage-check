5c83cfcde6c19530f7424bad697b47556f242d94
niblicat <maybenaebli@gmail.com>
2023-12-20 17:14:40 -0600

    make dnd work + param types

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-    import tasks from '$lib/tasks.json';
+    import Tasklist from './tasklist.svelte';
+import TaskList from './tasklist.svelte';
     import * as todo from './todo';
 
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Tasklist />
 
 <style>
 
---
+++ src/routes/tasklist.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import {flip} from "svelte/animate";
+    import {dndzone} from "svelte-dnd-action";
+
+    import tasks from '$lib/tasks.json';
+
+    var items = tasks;
+    var flipDurationMs = 200;
+
+    function handleDndConsider(e: CustomEvent) {
+        items = e.detail.items;
+    }
+    function handleDndFinalize(e: CustomEvent) {
+        items = e.detail.items;
+    }
+
+</script>
+
+<section use:dndzone="{{items, flipDurationMs}}" on:consider="{handleDndConsider}" on:finalize="{handleDndFinalize}">
+    {#each items as item(item.id)}
+    <div animate:flip="{{duration: flipDurationMs}}">{item.title}</div>
+    {/each}
+</section>
+
+<style>
+
+</style>
